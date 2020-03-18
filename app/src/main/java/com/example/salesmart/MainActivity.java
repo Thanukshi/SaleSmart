081c25d238ff97ab852c7f1df340f305a7165b85
Thanukshi <thanukshi1234@gmail.com>
2020-03-18 10:13:15 +0530

    SaleSmart_Mobile_App

---
+++ app/src/main/java/com/example/salesmart/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.salesmart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private  static int SPLASH_SCREEN = 5000;
+
+    ImageView splashImage;
+    Animation topAnim;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+
+        splashImage = findViewById(R.id.imageLoad);
+
+        splashImage.setAnimation(topAnim);
+
+
+       new Handler().postDelayed( new Runnable() {
+           @Override
+           public void run() {
+               Intent intent = new Intent( MainActivity.this, Login.class );
+               startActivity( intent );
+               finish();
+           }
+       },SPLASH_SCREEN);
+    }
+}
