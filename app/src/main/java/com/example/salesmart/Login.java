0dd3113c134e89899ac35a4032d5d2d990c977a0
Thanukshi <thanukshi1234@gmail.com>
2020-03-18 18:45:07 +0530

    SaleSmart_Mobile_App

--- app/src/main/java/com/example/salesmart/Login.java
+++ app/src/main/java/com/example/salesmart/Login.java
@@ -3,12 +3,34 @@ package com.example.salesmart;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class Login extends AppCompatActivity {
+
+    RelativeLayout relLay1, relLay2, relLay3;
+    Handler handler = new Handler();
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            relLay1.setVisibility( View.VISIBLE );
+            relLay2.setVisibility( View.VISIBLE );
+            relLay3.setVisibility( View.VISIBLE );
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login );
+        setContentView(R.layout.activity_login);
+
+        relLay1 = (RelativeLayout)findViewById( R.id.rl1Login );
+        relLay2 = (RelativeLayout)findViewById( R.id.rl2Login );
+        relLay3 = (RelativeLayout)findViewById( R.id.rl3Login );
+
+        handler.postDelayed( runnable, 1000 );
+
+
     }
 }
