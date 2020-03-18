d8b888d102f1a131b37aacb483feedaf5f67fc1f
Tharindu Sampath <tharindus816@gamail.com>
2020-03-19 04:32:40 +0530

    Sampath A.S.T(IT18060140)

---
+++ app/src/main/java/com/example/salesmart/PurchaseItem.java
@@ -0,0 +1,14 @@
+package com.example.sales_mart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PurchaseItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_purchase_item);
+    }
+}
