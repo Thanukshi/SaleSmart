1cc94a94649431e0cf8432673968986302791a21
Thanukshi <thanukshi1234@gmail.com>
2020-03-19 13:21:26 +0530

    SaleSmart_Mobile_App(IT-18202632)

---
+++ app/src/main/java/com/example/salesmart/Edit_Product.java
@@ -0,0 +1,14 @@
+package com.example.salesmart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Edit_Product extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit__product);
+    }
+}
